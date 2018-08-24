ecd36d7909100e6a26b3e44018276dba4e625b98
Ryan L. Bell <ryan.l.bell@gmail.com>
2018-08-24 08:49:05 -0400

    Fix loader check on Windows

--- index.js
+++ index.js
@@ -1,37 +1,46 @@
 const cloneDeep = require('lodash.clonedeep');
 
-const ruleChildren = (loader) => loader.use || loader.oneOf || Array.isArray(loader.loader) && loader.loader || [];
+const ruleChildren = loader =>
+  loader.use || loader.oneOf || (Array.isArray(loader.loader) && loader.loader) || [];
 
 const findIndexAndRules = (rulesSource, ruleMatcher) => {
-    let result = undefined;
-    const rules = Array.isArray(rulesSource) ? rulesSource : ruleChildren(rulesSource);
-    rules.some((rule, index) => result = ruleMatcher(rule) ? {index, rules} : findIndexAndRules(ruleChildren(rule), ruleMatcher));
-    return result;
-}
+  let result = undefined;
+  const rules = Array.isArray(rulesSource) ? rulesSource : ruleChildren(rulesSource);
+  rules.some(
+    (rule, index) =>
+      (result = ruleMatcher(rule)
+        ? { index, rules }
+        : findIndexAndRules(ruleChildren(rule), ruleMatcher)),
+  );
+  return result;
+};
 
 const findRule = (rulesSource, ruleMatcher) => {
-    const {index, rules} = findIndexAndRules(rulesSource, ruleMatcher);
-    return rules[index];
-}
+  const { index, rules } = findIndexAndRules(rulesSource, ruleMatcher);
+  return rules[index];
+};
 
-const cssRuleMatcher = (rule) => rule.test && String(rule.test) === String(/\.css$/);
+const cssRuleMatcher = rule => rule.test && String(rule.test) === String(/\.css$/);
 
-const createLoaderMatcher = (loader) => (rule) => rule.loader && rule.loader.indexOf(`/${loader}/`) !== -1;
+const createLoaderMatcher = loader => rule =>
+  rule.loader &&
+  (rule.loader.indexOf(`/${loader}/`) !== -1 || rule.loader.indexOf(`\\${loader}\\`) !== -1);
+
 const cssLoaderMatcher = createLoaderMatcher('css-loader');
 const postcssLoaderMatcher = createLoaderMatcher('postcss-loader');
 const fileLoaderMatcher = createLoaderMatcher('file-loader');
 
 const addAfterRule = (rulesSource, ruleMatcher, value) => {
-    const {index, rules} = findIndexAndRules(rulesSource, ruleMatcher);
-    rules.splice(index + 1, 0, value);
-}
+  const { index, rules } = findIndexAndRules(rulesSource, ruleMatcher);
+  rules.splice(index + 1, 0, value);
+};
 
 const addBeforeRule = (rulesSource, ruleMatcher, value) => {
-    const {index, rules} = findIndexAndRules(rulesSource, ruleMatcher);
-    rules.splice(index, 0, value);
-}
+  const { index, rules } = findIndexAndRules(rulesSource, ruleMatcher);
+  rules.splice(index, 0, value);
+};
 
-module.exports = function (config, env) {
+module.exports = function(config, env) {
   // Add CSSnext plugins
   const postcssLoader = findRule(config.module.rules, postcssLoaderMatcher);
   const oldPostcssPlugins = postcssLoader.options.plugins();
@@ -44,12 +53,12 @@ module.exports = function (config, env) {
   const postcssPlugins = [
     require('postcss-import'),
     require('postcss-url'),
-    require('postcss-cssnext')(autoprefixerOptions)
+    require('postcss-cssnext')(autoprefixerOptions),
   ].concat(oldPostcssPlugins);
-  const newPluginsFun = function () {
+  const newPluginsFun = function() {
     return postcssPlugins;
   }.bind(postcssLoader.options);
   postcssLoader.options.plugins = newPluginsFun;
 
-  return config
-}
+  return config;
+};
